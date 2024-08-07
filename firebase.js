329f155e6497cb233c047b94d83329f3d1e0b5d3
jguzmanps4 <jguzmanps4@gmail.com>
2024-08-07 15:36:34 -0400

    Updated firebase.js file

--- firebase.js
+++ firebase.js
@@ -14,13 +14,13 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfh8TtSM9EF89OGMb7BzFiIwmhbW8JxRM",
-  authDomain: "inventory-management-1f37a.firebaseapp.com",
-  projectId: "inventory-management-1f37a",
-  storageBucket: "inventory-management-1f37a.appspot.com",
-  messagingSenderId: "765551036163",
-  appId: "1:765551036163:web:94db557887874111464b36",
-  measurementId: "G-W4PES9FCE4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
